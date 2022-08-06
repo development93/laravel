b197cf03fca2c834a438a8d30c2a22a40e39145a
development93 <shashimehta93@gmail.com>
2022-08-06 17:54:45 +0530

    first commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,14 @@
+<footer>
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+        © 2020 Copyright:
+        <a class="text-white" href="">Learn</a>
+      </div>
+</footer>
+<style>
+    .custom-login{
+        margin-top: 100px;
+        margin-bottom: 300px;
+    }
+</style>
+</body>
+</html>
